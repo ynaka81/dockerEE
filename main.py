b18c11a0377b3fecf7ecd335e958faacf47c4377
YutaNakagawa <ytnkgw@gmail.com>
2015-09-23 23:00:39 +0900

    add process to change sys.argv[2] to abspath

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# import sys
-# sys.path.append('./')
+import os
+import sys
 from dockerEE.service import EnvironmentEmulationService
 
 if __name__ == "__main__":
+    # change 2nd argument to abspath
+    if len(sys.argv) >= 3 and os.path.isfile(sys.argv[2]):
+        sys.argv[2] = os.path.abspath(sys.argv[2])
+    # service action
     service = EnvironmentEmulationService('localhost', 'vagrant', 'vagrant')
     service.action()
